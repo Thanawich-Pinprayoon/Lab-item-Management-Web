0a76a411308b1a16eed71bdfa9042cba2ac0ab0e
Nattarat Serirat <fangy2543@hotmail.com>
2021-04-04 17:49:51 +0700

    update asp

---
+++ Controllers/SelectItemController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Lab_item_Management_Web.Models;
+
+namespace Lab_item_Management_Web.Controllers
+{
+    public class SelectItemController : Controller
+    {
+        private readonly ILogger<SelectItemController> _logger;
+
+        public SelectItemController(ILogger<SelectItemController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult SelectItem()
+        {
+            return View();
+        }
+
+        // public IActionResult Privacy()
+        // {
+        //     return View();
+        // }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
