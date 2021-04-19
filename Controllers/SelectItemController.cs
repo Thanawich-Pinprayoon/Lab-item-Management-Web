78ba38a7de5880747b34e1d9b69dcae80f4d6db1
Nattarat Serirat <fangy2543@hotmail.com>
2021-04-19 21:18:42 +0700

    Edit view

--- Controllers/SelectItemController.cs
+++ Controllers/SelectItemController.cs
@@ -49,7 +49,9 @@ namespace Lab_item_Management_Web.Controllers
                 Name = labs.Name,
                 Description = labs.Description,
                 Picture = labs.Picture,
+                ToolId= tools.Id,
                 ToolName = tools.Name,
+                ToolLabId= tools.LabID,
                 Amount = tools.ItemAmount,
                 ToolPicture = tools.Picture,
                 ItemDesc = tools.Description,
@@ -110,6 +112,61 @@ namespace Lab_item_Management_Web.Controllers
                 return Redirect($"/SelectItem/Index/{lab.Id}");
             }
             return View(lab);
+        }
+
+          // GET: User/Edit/5
+        public async Task<IActionResult> EditTool(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tool = await _context.Tool.FindAsync(id);
+            if (tool == null)
+            {
+                return NotFound();
+            }
+            return View(tool);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditTool(int id, [Bind("Id,Name,Description,Picture,ItemAmount")] Tool tool)
+        {
+            if (id != tool.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tool);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LabExists(tool.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return Redirect($"/SelectItem/Index/{tool.Id}");
+            }
+            return View(tool);
+        }
+        private bool ToolExists(int id)
+        {
+            return _context.Lab.Any(e => e.Id == id);
         }
         private bool LabExists(int id)
         {
